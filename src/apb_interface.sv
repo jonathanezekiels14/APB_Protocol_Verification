7c13d17f137275d4233212986e71ce739580e669
Jonathan Ezekiels <jonathanezekiels@feserver.mirafra.com>
2026-07-09 11:33:11 +0530

    Completed Interface

--- src/apb_interface.sv
+++ src/apb_interface.sv
@@ -1,24 +1,50 @@
 interface apb_interface (input bit PCLK, input bit PRESETn);
-	// INPUTS
+	// Brigde Signals
 	logic transfer;
 	logic write_read;
 	logic [`ADDR_WIDTH-1:0] addr_in;
-	logic [`DATA_WIDTH-1:0] w_data_in;
+	logic [`DATA_WIDTH-1:0] wdata_in;
 	logic [(`DATA_WIDTH/8)-1:0] strb_in;
-	logic [`DATA_WIDTH-1:0] PRDATA;
-	logic PREADY;
-	logic PSVERR;
-	
-	// OUTPUTS
+	logic error;
+	logic [`DATA_WIDTH-1:0] rdata_out;
+	logic transfer_done;
+
+	// Slave Signals
 	logic PSELx;
-	logic [(`DATA_WIDTH/8)-1:0] PSTRB;
 	logic PENABLE;
 	logic PWRITE;
+	logic [(`DATA_WIDTH/8)-1:0] PSTRB;
 	logic [`DATA_WIDTH-1:0] PWDATA;
 	logic [`ADDR_WIDTH-1:0] PADDR;
-	logic transfer_done;
-	logic error;
-	logic [`DATA_WIDTH-1:0] r_data_out;
+	logic [`DATA_WIDTH-1:0] PRDATA;
+	logic PREADY;
+	logic PSLVERR;
 
-	clocking 
+	// Clocking Blocks
+	
+	clocking bridge_drv_cb @(posedge PCLK);
+		default input #1step output #1ns;
+		output transfer,write_read,addr_in,wdata_in,strb_in;
+		input error,rdata_out,transfer_done;
+	endclocking 
+
+	clocking slave_drv_cb @(posedge PCLK);
+		default input #1step output #1ns;
+		output PRDATA,PREADY,PSLVERR;
+		input PSTRB,PSELx,PENABLE,PWRITE,PWDATA,PADDR;
+	endclocking
+	
+	clocking mon_cb @(posedge PCLK);
+		default input #1step;
+		input transfer,write_read,addr_in,wdata_in, strb_in, rdata_out, transfer_done, error;
+		input PADDR,PSEL,PENABLE,PWRITE,PWDATA,PSTRB,PRDATA,PREADY,PSLVERR;
+	endclocking
+
+	modport BRIDGE_DRV (clocking bridge_drv_cb, input PRESETn);
+	modport SLAVE_DRV (clocking slave_drv_cb, input PRESETn);
+	modport MON (clocking mon_cb,input PRESETn);
+
+endinterface
+
+
 
