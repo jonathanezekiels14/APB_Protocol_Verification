0fe27c18d730aa439667a5a7c0810633c6be4821
Jonathan Ezekiels <jonathanezekiels@feserver.mirafra.com>
2026-07-09 16:25:46 +0530

    Completed Slave Driver

--- src/apb_slave_driver.sv
+++ src/apb_slave_driver.sv
@@ -22,11 +22,50 @@ class apb_slave_driver;
 
 			fork
 				begin
+
+					do
+						@(vif.slave_drv_cb);
+					while(!(vif.slave_drv_cb.PSEL === 0 && vif.slave_drv_cb.PENABLE === 1));
+
+
 					gen_2_slave_drv.get(slave_trans);
 					drv_2_refput(slave_trans);
 
 					@(vif.slave_drv_cb);
-					
 
-				
+					if(slave_trans.wait_states > 0) begin
+						vif.slave_drv_cb.PREADY <= 0;
+						repeat(slave_trans.wait_states) @(vif.slave_drv_cb);
 
+					end
+
+					if(vif.slave_drv_cb.PWRITE == 0)
+						vif.slave_drv_cb.PRDATA <= slave_trans.PRDATA;
+					vif.slave_drv_cb.PREADY <= 1;
+					vif.slave_drv_cb.PSLVERR <= slave_trans.PSLVERR;
+
+					@(vif.slave_drv_cb);
+
+					vif.slave_drv_cb.PSLVERR <= 0;
+					vif.slave_drv_cb.PRDATA <= `hx;
+
+					$display("[DRIVER - SLAVE] Transaction : %d Completed Transfer",i);
+				end
+
+				begin
+					wait (vif.PRESETn == 0);
+				end
+
+			join_any
+
+			disable fork;
+
+			if(vif.PRESETn == 0) begin
+				$display("[DRIVER - SLAVE] Reset Asserted...");
+				vif.slave_drv_cb.PRDATA <= 0;
+				vif.slave_drv_cb.PREADY <= 0;
+				vif.slave_drv_cb.PSLVERR <= 0;
+			end
+		end
+	endtask
+endclass	
